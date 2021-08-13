d50574a9b8d7b2f5fd2b0fb6653de9faa0462cad
tiennv3 <nguyentien9500@gmail.com>
2021-08-13 16:39:37 +0700

    commit

--- src/database.js
+++ src/database.js
@@ -1,4 +1,12 @@
 module.exports = {
+  "0": {
+    "name": "Messi",
+    "birthday": "08-03",
+    'description': "A Cybloc is a Cyborg on the blockchain. Each Cybloc is represented as an NFT (Non-Fungible Token) which is a unique digital asset that lies on the blockchain. Each Cybloc has a unique ID which remains permanent within the game and on-chain - the lower the CyblocID, the closer that Cybloc is to genesis (the first batch of Cyblocs ever created).",
+    'external_link': "https://cyball.gitbook.io/cyball-cybloc-football/",
+    'seller_fee_basis_points': 400,
+    'fee_recipient': "0x854f882771b61b26e91F8644dc0c9c94301FaD2d"
+  },
   "1": {
     "name": "Chunky Monkey",
     "birthday": "10-18",
